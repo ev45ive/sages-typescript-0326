1411c6d2c2ddef8795542402cf1482cc421e06cd
Mateusz Kulesza <ev45ive@gmail.com>
2026-03-12 13:42:51 +0100

    Node Express http server

--- trener/2_backend/src/index.ts
+++ trener/2_backend/src/index.ts
@@ -1,4 +1,21 @@
-console.log("Hello NodeJs!");
-
-console.log(process.argv.slice(2));
+// console.log("Hello NodeJs!");
+// console.log(process.argv.slice(2));
 // console.log(process.env['PORT']);
+import express from 'express'
+
+const app = express(); 
+
+app.get("/", (req, res) => {
+  res.send(/* html */ `<h1>Hello!</h1>`);
+});
+
+const HOST = "localhost";
+const PORT = 3000;
+
+app.listen(PORT, HOST, (error) => {
+  if (error) {
+    console.log(`Error ${error.message}`);
+  } else {
+    console.log(`Listening on http://${HOST}:${PORT}/`);
+  }
+});
