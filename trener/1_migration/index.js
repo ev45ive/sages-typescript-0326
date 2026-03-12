feeca8ef3348b6eef6f7536d5bb4b8079c0fc3e1
Mateusz Kulesza <ev45ive@gmail.com>
2026-03-12 10:57:04 +0100

    Transpilation ES2020 -> ES5

--- trener/1_migration/index.js
+++ trener/1_migration/index.js
@@ -1,5 +1,22 @@
-function add(a, b) {
-    return a + b;
-}
-
-add(1, 2);
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NaszaKlasa = exports.showItem = void 0;
+var lib_1 = require("./lib");
+(0, lib_1.add)(1, 2);
+// getPersonInfo({}) // err
+var showItem = function (item) { return "".concat(item.name); };
+exports.showItem = showItem;
+var NaszaKlasa = /** @class */ (function () {
+    //   readonly #tajne = "sekret";
+    function NaszaKlasa(która) {
+        if (która === void 0) { która = "1A"; }
+        this.która = która;
+        this.dodajUcznia = function (imie) {
+            console.log("Dodaje ucznia ".concat(imie));
+        };
+        this.która;
+    }
+    NaszaKlasa.szkola = "Nasza szkola";
+    return NaszaKlasa;
+}());
+exports.NaszaKlasa = NaszaKlasa;
--- trener/1_migration/index.ts
+++ trener/1_migration/index.ts
@@ -1,13 +1,21 @@
 import { add, getPersonInfo } from "./lib";
 
-
 add(1, 2);
 
-// @ts-expect-error
-add('1', 2); // err?
-
-// @ts-ignore
-add('1', []); // erro?
-
-
 // getPersonInfo({}) // err
+
+export const showItem = (item: { name: string }) => `${item.name}`;
+
+export class NaszaKlasa {
+  static readonly szkola = "Nasza szkola";
+
+//   readonly #tajne = "sekret";
+
+  constructor(readonly która = "1A") {
+    this.która;
+  }
+
+  dodajUcznia = (imie: string) => {
+    console.log(`Dodaje ucznia ${imie}`);
+  };
+}
