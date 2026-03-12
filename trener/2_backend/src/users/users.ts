fce71652af14a44d945fabf2b943faf222f98402
Mateusz Kulesza <ev45ive@gmail.com>
2026-03-12 14:47:22 +0100

    Exercise: Add types to users[]

---
+++ trener/2_backend/src/users/users.ts
@@ -0,0 +1,20 @@
+export const users = [
+  {
+    id: "123",
+    name: "Alice",
+    color: "red",
+    pet: { name: "Cat" },
+  },
+  {
+    id: "234",
+    name: "Bob",
+    color: "blue",
+    pet: { name: "Dog" },
+  },
+  {
+    id: "345",
+    name: "Kate",
+    color: "green",
+    pet: { name: "Fish" },
+  },
+];
