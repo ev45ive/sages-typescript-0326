c480c526fb84440733f32c1fce2a94c1e81d81af
Mateusz Kulesza <ev45ive@gmail.com>
2026-03-12 15:16:39 +0100

    Freshness // Polymorphism - Information Hiding

--- trener/2_backend/src/users/users.ts
+++ trener/2_backend/src/users/users.ts
@@ -1,11 +1,13 @@
-export const users: {
+export interface User {
   id: string;
   name: string;
   color: string;
   pet?: {
     name: string;
   };
-}[] = [
+}
+
+export const users: User[] = [
   {
     id: "123",
     name: "Alice",
@@ -31,3 +33,33 @@ export const users: {
 // type Users = typeof users   // TS - { id: string; ...
 // type User = Users[number]
 // type Pet = User['pet'] // Type lookup
+
+export function getUserInfo(user: User) {}
+export function getUserPet(user: { pet: User["pet"] }) {
+  user.pet?.name;
+  // (user as User).name
+}
+
+// Freshness
+getUserInfo({ id: "123", name: "123", color: "123" /* , colour: 123 */ });
+
+// const u2: User = { id: "123", name: "123", color: "123", colour:123}
+
+// Class UserAggr
+const u3 = {
+  // IUserMeta
+  id: "123",
+  name: "123",
+  color: "123",
+
+  // IUserPet
+  pet: { name: "123" },
+
+  // IUserSocial
+  friends: [],
+  role: [],
+};
+
+// Polymorphism - Information Hiding
+getUserInfo(u3);
+getUserPet(u3);
