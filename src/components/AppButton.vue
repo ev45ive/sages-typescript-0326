f728881c5457a87f97c160f0d853e5646658bb8a
Mateusz Kulesza <ev45ive@gmail.com>
2026-03-13 11:58:23 +0100

    AppButton

---
+++ src/components/AppButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>
+    {{ $props.variant }}
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  /* TS compiletime */
+  variant: string
+}>(/* JS runtime validation */)
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/DesignView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-2xl">Design View</div>
+
+  <div class="grid gap-5">
+    <AppButton variant="test" class="bg-green-400 text-white"> Submit </AppButton>
+    <AppButton class="bg-red-400 text-white">Cancel</AppButton>
+    <AppButton class="bg-blue-400 text-white">Info</AppButton>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { default as AppButton } from '../components/AppButton.vue'
+</script>
+
+<style scoped></style>
