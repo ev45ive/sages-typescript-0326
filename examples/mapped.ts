0ab630c88686c700d50a4e10f15deb1cdb650532
Mateusz Kulesza <ev45ive@gmail.com>
2026-03-13 15:02:02 +0100

    Utility Types = type Partial<T> = { [k in keyof T]?: T[k] }

--- examples/mapped.ts
+++ examples/mapped.ts
@@ -1,6 +1,7 @@
 // oxlint-disable no-unused-vars
 // ---------
 // An index signature parameter type cannot be a literal type or generic type. Consider using a mapped object type instead
+export {}
 
 type TeamColors = 'red' | 'blue'
 
@@ -34,3 +35,49 @@ const players: TeamPlayers = {
     players: [],
   },
 }
+
+// ---
+
+type Album = {
+  album_type: string
+  total_tracks: number
+  available_markets: string[]
+  external_urls: {
+    spotify: string
+  }
+  href: string
+  id: string
+  images: {
+    url: string
+    height: number
+    width: number
+  }[]
+  name: string
+  release_date: string
+  release_date_precision: string
+  restrictions: {}
+  type: string
+}
+
+// Lookup
+type AlbumId = Album['id']
+type AlbumImage = Album['images'][number]
+
+// Mapped
+type keys = keyof Album
+
+// type PartialAlbum = {
+//   [k in keys]?: Album[k]
+// }
+
+// type PartialAlbum = {
+//   [k in keyof Album]?: Album[k]
+// }
+
+// function(T){ for (key in T) ... }
+
+// type Partial<T> = {
+//   [k in keyof T]?: T[k]
+// }
+
+type PartialAlbum = Partial<Album>
