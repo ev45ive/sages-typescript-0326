fb87b7d8ccc59a2aea086144242366fe9e7e734f
Mateusz Kulesza <ev45ive@gmail.com>
2026-03-13 16:34:06 +0100

    assertType<ExtractType<Example, string>, Expected, 'This is bad...'>()

--- examples/conditional.ts
+++ examples/conditional.ts
@@ -42,3 +42,37 @@ type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>
 type o = Omit<{ x: 1; y: 2 }, 'x' | 'z'>
 
 // Draft = Omit<Entity,'id'>
+
+// ---
+
+// Exercise: Extract keys by type
+type Example = { id: number; name: string; surname: string }
+type Expected = {
+  name: string
+  surname: string
+}
+type Equal<A, B> =
+  (<T>() => T extends A ? 1 : 0) extends <T>() => T extends B ? 1 : 0 ? true : false
+
+function assertType<A, B, E = 'Types are not equal'>(
+  ..._: Equal<A, B> extends true ? [] : [error: E]
+): void {}
+
+// { name: never }
+type ExtractByType1<T, VT> = {
+  [k in keyof T]: T[k] extends VT ? k : never
+}
+type e0 = ExtractByType1<Example, string>
+
+// never | "name" | "surname"
+type ExtractByType2<T, VT> = ExtractByType1<T, VT>[keyof T]
+
+type e1 = ExtractByType2<Example, string>
+
+type ExtractType<T, VT> = Pick<T, ExtractByType2<T, VT>>
+
+type e2 = ExtractType<Example, string> extends Expected ? true : 'Error'
+
+const Check1: Equal<e2, true> = true
+
+assertType<ExtractType<Example, string>, Expected, 'This is bad...'>()
