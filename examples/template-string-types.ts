31d59f4e8f22545d0c28ddf6cc991a0c444d8391
Mateusz Kulesza <ev45ive@gmail.com>
2026-03-13 14:26:45 +0100

    AutoComplete = 'Accept' | 'Authorization' | (string & {})

--- examples/template-string-types.ts
+++ examples/template-string-types.ts
@@ -23,3 +23,30 @@ type EventMap = {
 type EventHandlers = {
   [event in events]?: (e: EventTypes[event]) => void
 }
+
+// ---------
+
+// type AllowedHeaders = 'Accept' | 'Authorization' // Choice
+// type AllowedHeaders = 'Accept' | 'Authorization' | string // string
+
+// type AllowedHeaders = 'Accept' | 'Authorization' | `${'A' | 'B'}` // "Accept" | "Authorization" | "A" | "B"
+// type AllowedHeaders = 'Accept' | 'Authorization' | `X-${string}` // Template union
+// type AllowedHeaders = 'Accept' | 'Authorization' | (string & {})
+
+type AutoComplete = string & {}
+type AllowedHeaders = 'Accept' | 'Authorization' | AutoComplete
+
+type RequestConfig = {
+  headers: {
+    // [h in AllowedHeaders]?: string
+    [h in AllowedHeaders]: string
+  }
+}
+
+const req: RequestConfig = {
+  headers: {
+    Accept: '',
+    Authorization: '',
+    banana: 'OK',
+  },
+}
