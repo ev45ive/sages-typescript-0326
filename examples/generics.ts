f91d4e8e9b72dce31e3bb994afd98a2c932c5b8d
Mateusz Kulesza <ev45ive@gmail.com>
2026-03-13 15:19:25 +0100

    Useless generics - <T> - types relation

--- examples/generics.ts
+++ examples/generics.ts
@@ -1,3 +1,4 @@
+// oxlint-disable no-unused-expressions
 // oxlint-disable no-unused-vars
 export {}
 
@@ -12,3 +13,16 @@ b.value.królik.z.kapeusza()
 type TypedBox<T> = { value: T }
 
 const b2: TypedBox<string> = { value: '' }
+b2.value = ''
+b2.value // string
+
+// ---
+// Useless generics - <T> - types relation
+
+declare function parseJSON<T>(json: string): T
+/*            */ JSON.parse('') as {}
+
+declare function serializeJSON<T>(obj: T): string
+/*            */ JSON.stringify('') // string
+
+//
