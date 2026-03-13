2e174aacff1763a9a7d67657c4f235b0a9dd018a
Mateusz Kulesza <ev45ive@gmail.com>
2026-03-13 13:53:04 +0100

    Mapped Types - type Scores = { [color in TeamColors]: { color: color; score: number } }

---
+++ examples/mapped.ts
@@ -0,0 +1,10 @@
+// oxlint-disable no-unused-vars
+// ---------
+// An index signature parameter type cannot be a literal type or generic type. Consider using a mapped object type instead
+
+type TeamColors = 'red' | 'blue'
+
+// for team in teams { team.score }
+type Scores = {
+  [color in TeamColors]: { color: color; score: number }
+}
